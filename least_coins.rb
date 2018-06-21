988b337f50c079c05f126b6deb398f2bfff32977
GraceAHerr <mazyg321@gmail.com>
2018-06-21 15:51:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,6 @@
 #write out your code here
 
-def least_coins(coins)
+def least_coins(cents)
   coins = {:pennies => 0.01, :nickels => 0.05, :dimes => 0.10, :quarters => 0.25}
   loop do
     puts pennies = 0
