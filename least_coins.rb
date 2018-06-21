d9a834a3ece0787a4ffdbb7f5202809cdb9860f4
GraceAHerr <mazyg321@gmail.com>
2018-06-21 15:50:00 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,17 +1,16 @@
 #write out your code here
 
 def least_coins(cents)
-  coins = {:pennies => 1, :nickels => 5, :dimes => 10, :quarters => 25
+  coins = {:pennies => 1, :nickels => 5, :dimes => 10, :quarters => 25}
   loop do
     puts pennies = 0
     puts nickels = 0
     puts dimes = 0
     puts quarters = 0
   break
-}
   end
 end
 
 
 
-least_coins(30)
+least_coins(29)
